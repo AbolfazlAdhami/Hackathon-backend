efa5849e32785e06ba8d7b82062822fc60de0efa
AbolfazlAdhami <aadhami880@gmail.com>
2026-08-21 23:08:33 +0330

    init: add Skills and agent.md

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TransformInterceptor } from './utils/transform.interceptor';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
