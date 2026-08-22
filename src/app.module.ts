b03dc8069fa5e3e78f46c3daf582b020c2c98c89
AbolfazlAdhami <aadhami880@gmail.com>
2026-08-22 04:17:55 +0330

    feat: add rate limit with ai

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module, NestModule } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ArcjetGuard, ArcjetModule, fixedWindow, shield } from '@arcjet/nest';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
+  imports: [
+    ArcjetModule.forRoot({
+      isGlobal: true,
+      key: process.env.ARCJET_KEY!,
+      rules: [
+        shield({ mode: 'LIVE' }),
+        fixedWindow({
+          mode: 'DRY_RUN',
+          window: '60s',
+          max: 100,
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ArcjetGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure() {}
