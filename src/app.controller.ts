b3e18c51ee42333ac97be5dc2d86f1750e48a389
AbolfazlAdhami <aadhami880@gmail.com>
2026-08-23 05:56:32 +0330

    new: auth file and better auth fix import issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -6,6 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @AllowAnonymous()
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
+  app.enableCors;
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: () => new BadRequestException('Validation failed'),
+    }),
+  );
+  console.log(process.env.PORT);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
